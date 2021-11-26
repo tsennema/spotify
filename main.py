0ce060e0724ce9a8688c8f347b68f89d2bd21b9a
tsennema <89606946+tsennema@users.noreply.github.com>
2021-11-26 17:00:20 -0500

    first test code from youtube

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
+import requests
+import base64, json
+from secrets import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+authURL = "https://accounts.spotify.com/api/token"
+authHeader = {}
+authData = {}
 
+# Base64 Encode Client ID and Client Secret
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def getAccessToken(clientID, clientSecret):
+    message = f"{clientID}:{clientSecret}"
+    message_bytes = message.encode('ascii')
+    base64_bytes = base64.b64encode(message_bytes)
+    base64_message = base64_bytes.decode('ascii')
 
+    authHeader['Authorization'] = "Basic " + base64_message
+    authData['grant_type'] = "client_credentials"
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    res = requests.post(authURL, headers=authHeader, data=authData)
+    responseObject = res.json()
+    # print(json.dumps(responseObject, indent=2))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    accessToken = responseObject['access_token']
+
+    return accessToken
+
+def getPlaylistTracks(token, playlistID):
+    playlistEndPoint = f"https://api.spotify.com/v1/playlists/{playlistID}"
+
+    getHeader = {
+        "Authorization": "Bearer " + token
+    }
+    res = requests.get(playlistEndPoint, headers=getHeader)
+
+    playlistObject = res.json()
+
+    return playlistObject
+
+# API requests
+token = getAccessToken(clientID, clientSecret)
+playlistID = "5CORh2nLsbjOQQ4leKe5jN?si=9139e89eec2a4216"
+
+tracklist = getPlaylistTracks(token, playlistID)
+
+with open('tracklist.json', 'w') as f:
+    json.dump(tracklist, f)
+
+for t in tracklist['tracks']['items']:
+    print ('----')
+    for a in t['track']['artists']:
+        print(a['name'])
+    songName = t['track']['name']
+    print(songName)
