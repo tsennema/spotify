5c3e6a0ade47f6f1666911ceb35169e2f4b3dc00
tsennema <89606946+tsennema@users.noreply.github.com>
2021-11-29 14:08:01 -0500

    fixed it, but don't know how

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, url_for, session, redirect
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from secrets import likedID, likedSecret
+# from secrets import likedID, likedSecret
 
 app = Flask(__name__)
 
@@ -11,28 +11,26 @@ app.config['SESSION_COOKIE_NAME'] = 'Cookie'
 
 @app.route('/')
 def login():
-    return "test"
+    spOauth = createSpotifyOauth()
+    authURL = spOauth.get_authorize_url()
+    return redirect(authURL)
 
 
-#     spOauth = createSpotifyOauth()
-#     authURL = spOauth.get_authorize_url()
-#     return redirect(authURL)
-#
-#
-# @app.route('/redirect')
-# def redirectPage():
-#     return "redirect"
-#
-#
+@app.route('/redirect')
+def redirectPage():
+    return "redirect"
+
+
 @app.route('/getLiked')
 def getLiked():
     return "test"
-#
-#
-# def createSpotifyOauth():
-#     return SpotifyOAuth(
-#         client_id=likedID,
-#         client_secret=likedSecret,
-#         redirect_uri=url_for('redirectPage', _external=True),
-#         scope='user-library-read'
-#     )
+
+likedID = "d16c203095be4fdd8416513097acbcb5"
+likedSecret = "ef2088df16d44622a652c2364b26d99f"
+def createSpotifyOauth():
+    return SpotifyOAuth(
+        client_id=likedID,
+        client_secret=likedSecret,
+        redirect_uri=url_for('redirectPage', _external=True),
+        scope='user-library-read'
+    )
