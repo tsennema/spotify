9b1395b9f631bcad16bc43b30d96e066e7ef6a50
tsennema <89606946+tsennema@users.noreply.github.com>
2021-11-29 17:24:56 -0500

    broke it again, and don't know why

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, url_for, session, redirect
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import time
 # from secrets import likedID, likedSecret
 
 app = Flask(__name__)
 
 app.secret_key = "OsdfdEdfdI234D"
 app.config['SESSION_COOKIE_NAME'] = 'Cookie'
-
+TOKENINFO = "token_info"
 
 @app.route('/')
 def login():
@@ -18,12 +19,36 @@ def login():
 
 @app.route('/redirect')
 def redirectPage():
-    return "redirect"
+    spOauth = createSpotifyOauth()
+    session.clear()
+    code = request.args.get('code')
+    tokenInfo = spOauth.get_access_token(code)
+    session[TOKENINFO] = tokenInfo
+    return redirect(url_for('getLiked', _external=True))
 
 
 @app.route('/getLiked')
 def getLiked():
-    return "test"
+    try:
+        tokenInfo = getToken()
+    except:
+        print("user not logged in")
+        redirect("/")
+    return "help meee"
+    #sp = spotipy.Spotify(auth=tokenInfo['access_token'])
+    return str(sp.current_user_saved_tracks(limit=50, offset=0)['items'][0])
+
+def getToken():
+    tokenInfo = session.get(TOKENINFO, None)
+    if not tokenInfo:
+        raise "exception"
+    now = int(time.time)
+    isExpired = tokenInfo['expires_at'] - now < 60
+    if (isExpired):
+        spOauth = createSpotifyOauth()
+        tokenInfo = spOauth.refresh_access_token(tokenInfo['refresh_token'])
+    return tokenInfo
+
 
 likedID = "d16c203095be4fdd8416513097acbcb5"
 likedSecret = "ef2088df16d44622a652c2364b26d99f"
