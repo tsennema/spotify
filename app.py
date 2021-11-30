3f6aa75e5156edd0f5602204ff1f7a41cc4aad2e
tsennema <89606946+tsennema@users.noreply.github.com>
2021-11-30 14:25:21 -0500

    followed youtube to conclusion, think I get it now I can get the list of songs, but i'll probably need their ids

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, url_for, session, redirect
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from flask import Flask, request, url_for, session, redirect
+import json
 import time
 # from secrets import likedID, likedSecret
 
@@ -12,50 +13,87 @@ TOKENINFO = "token_info"
 
 @app.route('/')
 def login():
-    spOauth = createSpotifyOauth()
-    authURL = spOauth.get_authorize_url()
-    return redirect(authURL)
+    print("login")
+    sp_oauth = create_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
+    # print(auth_url)
+    return redirect(auth_url)
+
+@app.route('/logout')
+def logout():
+    print("logout")
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect('/')
 
 
-@app.route('/redirect')
-def redirectPage():
-    spOauth = createSpotifyOauth()
+@app.route('/authorize')
+def authorize():
+    print("authorize")
+    sp_oauth = create_spotify_oauth()
     session.clear()
     code = request.args.get('code')
-    tokenInfo = spOauth.get_access_token(code)
-    session[TOKENINFO] = tokenInfo
-    return redirect(url_for('getLiked', _external=True))
+    token_info = sp_oauth.get_access_token(code)
+    session["token_info"] = token_info
+    return redirect("/getTracks")
 
 
-@app.route('/getLiked')
-def getLiked():
-    try:
-        tokenInfo = getToken()
-    except:
-        print("user not logged in")
-        redirect("/")
-    return "help meee"
-    #sp = spotipy.Spotify(auth=tokenInfo['access_token'])
-    return str(sp.current_user_saved_tracks(limit=50, offset=0)['items'][0])
+@app.route('/getTracks')
+def get_all_tracks():
+    print("getTracks")
+    session['token_info'], authorized = get_token()
+    session.modified = True
+    if not authorized:
+        return redirect('/')
+    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+    results = []
+    count = 0
+    while True:
+        offset = count * 50
+        count += 1
+        curGroup = sp.current_user_saved_tracks(limit=50, offset=offset)['items']
+        for idx, item in enumerate(curGroup):
+            track = item['track']
+            val = track['name'] + " - " + track['artists'][0]['name']
+            results += [val]
+        if (len(curGroup) < 50):
+            break
+    print(results)
+    return str(len(results))
+    # df = pd.DataFrame(results, columns=["song names"])
+    # df.to_csv('songs.csv', index=False)
+    return "done"
 
-def getToken():
-    tokenInfo = session.get(TOKENINFO, None)
-    if not tokenInfo:
-        raise "exception"
-    now = int(time.time)
-    isExpired = tokenInfo['expires_at'] - now < 60
-    if (isExpired):
-        spOauth = createSpotifyOauth()
-        tokenInfo = spOauth.refresh_access_token(tokenInfo['refresh_token'])
-    return tokenInfo
+# Checks to see if token is valid and gets a new token if not
+def get_token():
+    print("gettoken")
+    token_valid = False
+    token_info = session.get("token_info", {})
+
+    # Checking if the session already has a token stored
+    if not (session.get('token_info', False)):
+        token_valid = False
+        return token_info, token_valid
+
+    # Checking if token has expired
+    now = int(time.time())
+    is_token_expired = session.get('token_info').get('expires_at') - now < 60
+
+    # Refreshing token if it has expired
+    if (is_token_expired):
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
+
+    token_valid = True
+    return token_info, token_valid
 
 
 likedID = "d16c203095be4fdd8416513097acbcb5"
 likedSecret = "ef2088df16d44622a652c2364b26d99f"
-def createSpotifyOauth():
+
+def create_spotify_oauth():
     return SpotifyOAuth(
-        client_id=likedID,
-        client_secret=likedSecret,
-        redirect_uri=url_for('redirectPage', _external=True),
-        scope='user-library-read'
-    )
+            client_id=likedID,
+            client_secret=likedSecret,
+            redirect_uri=url_for('authorize', _external=True),
+            scope="user-library-read")
